34eee6ef04f749d916f99660163284500d93464b
Michiel Mayer <michiel.mayer@kvk.nl>
2025-08-11 10:51:46 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Example of the Wallet Connect Button in a Vue app 
+
+This repo shows an example of the integration of the Wallet Connect Button in a Vue app, only using the frontend. It is based on the standard Vue 3 + Vite setup. For further explanation and documentation, visit: https://wallet-connect.eu
+
+# Vue 3 + Vite
+
+This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+
+Learn more about IDE Support for Vue in the [Vue Docs Scaling up Guide](https://vuejs.org/guide/scaling-up/tooling.html#ide-support).
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <WalletConnectButton
+      client-id="nlw_358b64b727fff6d0b03b7b29edb7da00"
+      api-key="78396e965f9698aed80603ab517424bcc8f62b20b2e7e6d0a2ee69f6b962cd25"
+      wallet-connect-host="http://localhost:3021"
+      label="Deel gegevens met je wallet (local)"
+      @success="handleSuccess"
+    />
+
+    <WalletConnectButton
+      client-id="nlw_fe98ca6b35b25dbad0717ebb184dbe68"
+      api-key="27ff4d0ceb03b0cd02a4b4d6fe4fe6436791223be38823bc97b8319a5991c769"
+      label="Deel gegevens met je wallet (via wallet-connect.eu)"
+      @success="handleSuccess"
+    />
+
+    <div v-if="attributes">
+      <h3>Received attributes:</h3>
+      <pre>{{ JSON.stringify(attributes, null, 2) }}</pre>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import WalletConnectButton from "wallet-connect-button-vue"
+
+const attributes = ref(null)
+
+const handleSuccess = (attrs) => {
+  attributes.value = attrs
+}
+</script>
